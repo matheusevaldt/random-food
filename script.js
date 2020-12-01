b4a086f8c8e05da1d1af585c89aed0fec9614005
Matheus Evaldt <evaldtmatheus@gmail.com>
2020-11-30 21:20:08 -0300

    Starting to work in the main functionalities

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+const buttonDisplayAddFood = document.querySelector('.button-display-add-food');
+const formAddFood = document.querySelector('.form-add-food');
 const inputAddFood = document.querySelector('.input-add-food');
 const buttonAddFood = document.querySelector('.button-add-food');
 const amountOfFoods = document.querySelector('.amount-of-foods');
 const listOfFoods = document.querySelector('.list-of-foods');
+const buttonGetFood = document.querySelector('.button-get-food');
+const foodSelected = document.querySelector('.food-selected');
 
 let foods = [];
 let idFood = 0;
 
 buttonAddFood.disabled = true;
 
+buttonDisplayAddFood.addEventListener('click', displayAddFood);
 inputAddFood.addEventListener('input', statusButtonAddFood);
 buttonAddFood.addEventListener('click', event => event.preventDefault());
 buttonAddFood.addEventListener('click', addFood);
+buttonGetFood.addEventListener('click', getFood);
+
+function displayAddFood() {
+    buttonDisplayAddFood.style.display = 'none';
+    formAddFood.style.display = 'block';
+}
 
 function statusButtonAddFood() {
     if (inputAddFood.value.length !== 0) {
@@ -29,6 +40,7 @@ function addFood() {
     updateAmountOfFoods();
     updateListOfFoods();
     resetAddFood();
+    if (foods.length === 1) displayGetFoodButton();
 }
 
 function updateAmountOfFoods() {
@@ -38,17 +50,21 @@ function updateAmountOfFoods() {
 
 function updateListOfFoods() {
     resetListOfFoods();
-    foods.map((food, index) => {
-        const nameOfTheFood = document.createElement('li');
-        const period = foods.length - 1 === index ? '.' : ''
+    foods.map((name, index) => {
+        const li = document.createElement('li');
+        const food = `<span class="food-highlight">${name.food}</span>`;
+        const period = foods.length - 1 === index ? '.' : '';
         if (index === 0) {
-            nameOfTheFood.innerHTML = `${food.food}${period}`;
+            li.innerHTML = `${food}${period}`;
         } else {
-            nameOfTheFood.innerHTML = `, ${food.food}${period}`;
+            if (foods.length - 1 === index) {
+                li.innerHTML = ` and ${food}${period}`;
+            } else {
+                li.innerHTML = `, ${food}${period}`;
+            }
         }
-        listOfFoods.appendChild(nameOfTheFood);
+        listOfFoods.appendChild(li);
     });
-
 }
 
 function resetListOfFoods() {
@@ -61,4 +77,23 @@ function resetAddFood() {
     inputAddFood.value = '';
     buttonAddFood.disabled = true;
     buttonAddFood.classList.remove('button-add-food-enabled')
+}
+
+function displayGetFoodButton() {
+    buttonGetFood.style.display = 'block';
+    console.log('DISPLAY GET FOOD BUTTON');
+}
+
+function getFood() {
+    shuffleFoods();
+    // const food = foods[Math.floor(Math.random() * foods.length)];
+    // console.log(food.food);
+}
+
+function shuffleFoods() {
+    for (let i = 0; i < foods.length; i++) {
+        setTimeout(() => {
+            foodSelected.innerHTML = foods[i].food;
+        }, i * 2000);
+    }
 }
